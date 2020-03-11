912619a4adfbaa1605704f34ebf0cbfae01de7f6
Hristo Dzhambov and Zakkrey Short <hristo@>
2020-03-11 11:08:51 -0700

    pass test to get description

--- PackerTracker.Tests/ModelTests/ItemTests.cs
+++ PackerTracker.Tests/ModelTests/ItemTests.cs
@@ -18,5 +18,18 @@ namespace Packer.Tests
             Tracker newTracker = new Tracker("test","test","test","test");
             Assert.AreEqual(typeof(Tracker), newTracker.GetType());
         }
+        [TestMethod]
+        public void GetDescription_Description_String()
+        {
+            string tripType = "Beach";
+            string gear = "Umbrella";
+            string destination = "Costa Rica";
+            string directions = "Follow your instinct";
+
+            Tracker newTracker = new Tracker(tripType, gear, destination, directions);
+            object result = newTracker;
+
+            Assert.AreEqual(newTracker, result);
+        }
     }
 }
